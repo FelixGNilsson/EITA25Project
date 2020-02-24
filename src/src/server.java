11ba5909b6a26790c188acf02a209b5ef3d63f64
Felan <felan747@hotmail.com>
2020-02-24 09:48:52 +0100

    Some init structure

--- src/src/server.java
+++ src/src/server.java
@@ -84,8 +84,8 @@ public class server implements Runnable {
 				KeyStore ts = KeyStore.getInstance("JKS");
                 char[] password = "password".toCharArray();
 
-                ks.load(new FileInputStream("serverkeystore"), password);  // keystore password (storepass)
-                ts.load(new FileInputStream("servertruststore"), password); // truststore password (storepass)
+                ks.load(new FileInputStream("projekt1/serverkeystore"), password);  // keystore password (storepass)
+                ts.load(new FileInputStream("projekt1/servertruststore"), password); // truststore password (storepass)
                 kmf.init(ks, password); // certificate password (keypass)
                 tmf.init(ts);  // possible to use keystore as truststore here
                 ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
