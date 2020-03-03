adf4aec9d2ebcf112e353ee815498af84d0a3727
Ekreus <al96tjatte>
2020-03-03 14:58:13 +0100

    Fixed createCert.sh script

--- Certificates/createCert.sh
+++ Certificates/createCert.sh
@@ -3,13 +3,13 @@
 read -p "enter name of client: " clientName
 mkdir -p "client/$clientName"
 
-keytool -import -file Certificates/public/CA.pem -alias CA -keystore TrustStore
+keytool -import -file public/CA.pem -alias CA -keystore TrustStore
 keytool -keystore KeyStore -genkey -alias $clientName
 keytool -certreq -alias $clientName -keystore KeyStore -file $clientName.csr
 
-openssl x509 -CA Certificates/public/CA.pem -CAkey Certificates/private/cakey.pem -CAserial Certificates/public/CA.srl -req -in $clientName.csr -out $clientName.pem -days 100
+openssl x509 -CA public/CA.pem -CAkey private/cakey.pem -CAserial public/CA.srl -req -in $clientName.csr -out $clientName.pem -days 100
 
-keytool -import -trustcacerts -alias root -file Certificates/public/CA.pem -keystore KeyStore
+keytool -import -trustcacerts -alias root -file public/CA.pem -keystore KeyStore
 keytool -import -trustcacerts -alias $clientName -file $clientName.pem -keystore KeyStore
 
 mv TrustStore KeyStore $clientName.csr $clientName.pem client/$clientName/ 
