86a5052b8c0cd0ed3aaef8c755702c83ca88eb94
Ekreus <al96tjatte>
2020-03-03 15:45:56 +0100

    Changed so generatePasswdHash take encoded byte[] (In other words take a String instead of a byte[]) and made all methods public static

--- src/src/PasswordManager.java
+++ src/src/PasswordManager.java
@@ -1,10 +1,11 @@
+import javax.xml.bind.DatatypeConverter;
 import java.security.*;
 
 public class PasswordManager {
 
-    private String generatePasswdHash(String passwd, byte[] salt) throws NoSuchAlgorithmException {
+    public static String generatePasswdHash(String passwd, String salt) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(salt);
+        md.update(DatatypeConverter.parseBase64Binary(salt));
         byte[] bytes = md.digest(passwd.getBytes());
         StringBuilder sb = new StringBuilder();
         for(int i=0; i< bytes.length ;i++)
@@ -16,11 +17,11 @@ public class PasswordManager {
     }
 
 
-    private static byte[] getSalt() throws NoSuchAlgorithmException
+    public static String getSalt() throws NoSuchAlgorithmException
     {
         SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
         byte[] salt = new byte[16];
         sr.nextBytes(salt);
-        return salt;
+        return DatatypeConverter.printBase64Binary(salt);
     }
 }
