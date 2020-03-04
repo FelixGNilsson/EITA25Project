6df15ed9792cdf371ca2fb6319b565a92efdb5b1
Felan <felan747@hotmail.com>
2020-03-04 10:37:28 +0100

    lscmd fix

--- src/src/ServerUtils.java
+++ src/src/ServerUtils.java
@@ -70,7 +70,7 @@ public class ServerUtils {
             return db.getUsers();
         }
         if(command[0].equals("lscmd")){
-            return "ls, modify, delete, mkJournal, listJournals, listLogs, listUsers";
+            return "ls \n modify JournalID newStatus \n delete Patient \n mkJournal patient nurse illness pwd\n listJournals\n listLogs\n listUsers";
         }
         return "Unknown command";
     }
